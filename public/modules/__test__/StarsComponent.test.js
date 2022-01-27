24649c356c4a296d3a83473d0ce38cd726f2c859
Albert Domínguez <albertdc1984@gmail.com>
2022-01-27 21:14:44 +0100

    100% test coverage

--- public/modules/__test__/StarsComponent.test.js
+++ public/modules/__test__/StarsComponent.test.js
@@ -20,4 +20,14 @@ describe("Given a StarsComponent class", () => {
       expect(htmlUl).toBe(5);
     });
   });
+  describe("When it receives a score of 5", () => {
+    test("Then it should render 5 star icons", () => {
+      const parentComponent = document.createElement("div");
+      const score = 5;
+      new StarsComponent(parentComponent, score);
+      const htmlUl = parentComponent.querySelectorAll("li").length;
+
+      expect(htmlUl).toBe(5);
+    });
+  });
 });
